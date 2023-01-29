60371e874964070fee748b0ca395d6304ad704a7
ramon6767 <ramon.justis.ortega@gmail.com>
2023-01-29 13:55:45 -0800

    add ability to read subject from command line using -i flag. Example: python3 keyToRun.py -m cars

--- app/keyToRun.py
+++ app/keyToRun.py
@@ -1,7 +1,25 @@
 import os
 import openai
+import argparse
+
+def main():
+    print("Running keyToRun.py")
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--input", "-i", type=str, required=True)
+    args = parser.parse_args()
+    user_input = args.input
+    print(f"User input: {user_input}")
+    pass
 
 # Load your API key from an environment variable or secret management service
-openai.api_key = os.getenv("OPENAI_API_KEY")
+# openai.api_key = os.getenv("OPENAI_API_KEY")
 
-response = openai.Completion.create(model="text-davinci-003", prompt="Say this is a test", temperature=0, max_tokens=7)
+# subject = "wheels"
+# prompt = f"Generate upbeat branding snippet for {subject}"
+
+# response = openai.Completion.create(model="text-davinci-003", prompt=prompt, temperature=0.5, max_tokens=100)
+# print(response)
+
+if __name__ == "__main__":
+    main()
