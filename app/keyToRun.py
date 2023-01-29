469d611efb8cf7af3e30dac6cfb91d714a87f60a
ramon6767 <ramon.justis.ortega@gmail.com>
2023-01-29 14:11:00 -0800

    link user command line prompt subject to openAI to generate a branding snippet for user subject

--- app/keyToRun.py
+++ app/keyToRun.py
@@ -10,16 +10,16 @@ def main():
     args = parser.parse_args()
     user_input = args.input
     print(f"User input: {user_input}")
+    generate_branding_snippet(user_input)
     pass
 
-# Load your API key from an environment variable or secret management service
-# openai.api_key = os.getenv("OPENAI_API_KEY")
-
-# subject = "wheels"
-# prompt = f"Generate upbeat branding snippet for {subject}"
-
-# response = openai.Completion.create(model="text-davinci-003", prompt=prompt, temperature=0.5, max_tokens=100)
-# print(response)
+def generate_branding_snippet(prompt: str):
+    # Load your API key from an environment variable or secret management service
+    openai.api_key = os.getenv("OPENAI_API_KEY")
+    enriched_prompt = f"Generate upbeat branding snippet for {prompt}: "
+    response = openai.Completion.create(model="text-davinci-003", prompt=enriched_prompt, temperature=0.5, max_tokens=100)
+    
+    print(response)
 
 if __name__ == "__main__":
     main()
