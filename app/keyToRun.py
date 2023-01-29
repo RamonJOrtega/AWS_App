e476b0c4580814d1a7ab59f175ea288cafc73ac5
ramon6767 <ramon.justis.ortega@gmail.com>
2023-01-29 14:58:42 -0800

    gets branding snippet paragraph in addition to keywords for brand

--- app/keyToRun.py
+++ app/keyToRun.py
@@ -1,6 +1,7 @@
 import os
 import openai
 import argparse
+import re
 
 def main():
     print("Running keyToRun.py")
@@ -11,26 +12,42 @@ def main():
     user_input = args.input
 
     print(f"User input: {user_input}")
-    result = generate_branding_snippet(user_input)
+    branding_result = generate_branding_snippet(user_input)
+    keywords_result = generate_keywords(user_input)
+    print(branding_result)
+    print(keywords_result)
+
+def generate_keywords(prompt: str) -> str:
+    # Load your API key from an environment variable or secret management service
+    openai.api_key = os.getenv("OPENAI_API_KEY")
+    enriched_prompt = f"Generate related branding keywords for {prompt}: "
+    response = openai.Completion.create(model="text-davinci-003", prompt=enriched_prompt, temperature=0.1, max_tokens=64)
+
+    #extract output text.
+    keywords_text: str = response["choices"][0]["text"]
+    #strip whitespace
+    keywords_text = keywords_text.strip()
+ 
+    return keywords_text
+
 
 def generate_branding_snippet(prompt: str) -> str:
     # Load your API key from an environment variable or secret management service
     openai.api_key = os.getenv("OPENAI_API_KEY")
     enriched_prompt = f"Generate upbeat branding snippet for {prompt}: "
-    response = openai.Completion.create(model="text-davinci-003", prompt=enriched_prompt, temperature=0.5, max_tokens=32)
+    response = openai.Completion.create(model="text-davinci-003", prompt=enriched_prompt, temperature=0.1, max_tokens=200)
     
     #extract output text.
     branding_text: str = response["choices"][0]["text"]
 
     #strip whitespace
     branding_text = branding_text.strip()
-    # add ... to truncated statementss
-    last_char = branding_text[-1]
-
+    # add ... to truncated statements
+    last_char = branding_text[-2]
     if last_char not in {".", "!", "?"}:
         branding_text += "..."
 
-    print(branding_text)
+    return branding_text
 
 if __name__ == "__main__":
     main()
