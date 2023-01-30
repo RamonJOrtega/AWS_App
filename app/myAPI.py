c15d132bd72a9abefd4b148e0fdc06d3776ac336
ramon6767 <ramon.justis.ortega@gmail.com>
2023-01-30 15:34:11 -0800

    adds the /genenerate_snippet_and_keywords endpoint to myAPI to allow user to get snippet and keyword json response from the API endpoint

--- app/myAPI.py
+++ app/myAPI.py
@@ -13,3 +13,9 @@ async def generate_snippet_api(prompt: str):
 async def generate_keywords_api(prompt: str):
     keywords = generate_keywords(prompt)
     return {"keywords": keywords}
+
+@app.get("/generate_snippet_and_keywords")
+async def generate_keywords_api(prompt: str):
+    snippet = generate_branding_snippet(prompt)
+    keywords = generate_keywords(prompt)
+    return {"snippet": snippet, "keywords": keywords}
