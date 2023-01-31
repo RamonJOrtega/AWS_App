f4eff520b343cdaf3400bda49aab83c12dafbd2b
ramon6767 <ramon.justis.ortega@gmail.com>
2023-01-31 10:35:28 -0800

    adds AWS CDK, AWS CLI, Typescript, and puts all related files in the infrastructure file folder

--- app/myAPI.py
+++ app/myAPI.py
@@ -25,5 +25,5 @@ async def generate_keywords_api(prompt: str):
 
 def validate_input_length(prompt: str):
     if len(prompt) >= MAX_INPUT_LENGTH:
-        raise HTTPException(status_code=400, detail= f"Input length is too long. Must be under {MAX_INPUT_LENGTH}")
+        raise HTTPException(status_code=400, detail= f"Input length is too long. Must be under {MAX_INPUT_LENGTH} characters")
     pass
