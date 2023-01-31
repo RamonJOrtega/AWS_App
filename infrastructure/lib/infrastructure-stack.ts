0fc913dc36faabc8b38f4f241c08d30cb48ca070
ramon6767 <ramon.justis.ortega@gmail.com>
2023-01-31 14:11:59 -0800

    add layer object which is a lambda.Layerversion that used the code form the asset coming from the lambda_base_layer/layer.zip file and the compatible runtime of python3.9. Finally add the layer to the apiLambda object by adding it as an array

--- infrastructure/lib/infrastructure-stack.ts
+++ infrastructure/lib/infrastructure-stack.ts
@@ -6,10 +6,16 @@ export class InfrastructureStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    const layer = new lambda.LayerVersion(this, "BaseLayer", {
+      code: lambda.Code.fromAsset("lambda_base_layer/layer.zip"),
+      compatibleRuntimes: [lambda.Runtime.PYTHON_3_9],
+    });
+
     const apiLambda = new lambda.Function(this, "ApiFunction", {
       runtime: lambda.Runtime.PYTHON_3_9,
       code: lambda.Code.fromAsset("../app/"),
-      handler: "myAPI.handler"
+      handler: "myAPI.handler",
+      layers: [layer,]
     });
   }
 }
