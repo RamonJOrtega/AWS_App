a2eb08d6f4db3eeb7f8a2ceb59f623ec0a21f3b8
ramon6767 <ramon.justis.ortega@gmail.com>
2023-02-01 18:07:57 -0800

    add the output css files to attempt to host remotely on varcel

--- brand-helper-site/pages/index.tsx
+++ brand-helper-site/pages/index.tsx
@@ -14,6 +14,7 @@ export default function Home() {
         <meta name="description" content="Generate brand pitches for you product" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
+        <link href="/dist/output.css" rel="stylesheet"></link>
       </Head>
       
       <BrandHelper />
