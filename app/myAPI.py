366c305fbe933b838c7cd5d15353ead3a0770254
RamonJOrtega <ramon.justis.ortega@gmail.com>
2024-02-17 12:12:38 -0800

    remove async calls from URI functions because they aren't allowed by openAI

--- app/myAPI.py
+++ app/myAPI.py
@@ -1,8 +1,8 @@
 from fastapi import FastAPI, HTTPException
-from getAPIBrandingDataFromOpenAI import generate_branding_snippet, generate_keywords
+from getAPIBrandingDataFromOpenAI import generate_branding_snippet, generate_keywords, generate_snippet_and_keywords
 from mangum import Mangum
 from fastapi.middleware.cors import CORSMiddleware
-
+ 
 app = FastAPI()
 handler = Mangum(app)
 MAX_INPUT_LENGTH=32
@@ -17,24 +17,22 @@ app.add_middleware(
 )
 
 @app.get("/generate_snippet")
-async def generate_snippet_api(prompt: str):
+def generate_snippet_api(prompt: str):
     validate_input_length(prompt)
     snippet = generate_branding_snippet(prompt)
     return {"snippet": snippet, "keywords": []}
 
 @app.get("/generate_keywords")
-async def generate_keywords_api(prompt: str):
+def generate_keywords_api(prompt: str):
     validate_input_length(prompt)
     keywords = generate_keywords(prompt)
     return {"snippet": None, "keywords": keywords}
 
 @app.get("/generate_snippet_and_keywords")
-async def generate_keywords_api(prompt: str):
+def generate_keywords_api(prompt: str):
     validate_input_length(prompt)
-    keywords = generate_keywords(prompt)
-    snippet = generate_branding_snippet(prompt)
+    return generate_snippet_and_keywords(prompt)
 
-    return {"snippet": snippet, "keywords": keywords}
 
 def validate_input_length(prompt: str):
     if len(prompt) >= MAX_INPUT_LENGTH:
